afd6881a26025924af165cdf78e4e08647b9078c
Chandler Davis <davisc10@miamioh.edu>
2020-10-22 13:51:11 -0400

    Minor refactoring

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,6 +1,8 @@
+import * as firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // safe to expose publicly
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCrxI0frxlzVZBqg85TTo7xkbyjyVRBV4E",
   authDomain: "reshawk-app.firebaseapp.com",
   databaseURL: "https://reshawk-app.firebaseio.com",
@@ -10,3 +12,11 @@ export const firebaseConfig = {
   appId: "1:725106275678:web:87a740653ae594e92fcbcf",
   measurementId: "G-TC0203ZNT3",
 };
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
--- src/pages/login/login.page.jsx
+++ src/pages/login/login.page.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 
-import * as firebase from "firebase";
-
-import { firebaseConfig } from "../../firebase/firebase.util";
-
-firebase.initializeApp(firebaseConfig);
-
-var provider = new firebase.auth.GoogleAuthProvider();
+import { signInWithGoogle } from "../../firebase/firebase.util";
 
 const onLoginClick = () => {
-  firebase
-    .auth()
-    .signInWithPopup(provider)
+  signInWithGoogle()
     .then(function (result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
+
+      console.log(user);
 
       // ...
     })
