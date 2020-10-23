8ae3de8bab11b23b5aa1edb545ca0ce67c91f1a8
Chandler Davis <davisc10@miamioh.edu>
2020-10-23 02:33:21 -0400

    ui tweaks

--- src/pages/invite-link/invite-link.page.jsx
+++ src/pages/invite-link/invite-link.page.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { useAuth } from "../../firebase/firebase.util";
 
+import { PageContainer, CodeContainer } from "./invite-link.styles";
+
 const InviteLinkPage = () => {
   const auth = useAuth();
 
-  if (!auth.authorized) {
-    return (
-      <div>
-        <h1>you are not authorized</h1>
-        <button onClick={auth.signOut}>sign out</button>
-      </div>
-    );
-  }
+  const unauthorizedUI = () => (
+    <div>
+      <h3>
+        Whoa! We don't have you in our system. Contact your RA or supervisor!
+      </h3>
+      <button onClick={auth.signOut}>Go Back</button>
+    </div>
+  );
+
+  const authorizedUI = () => (
+    <div>
+      <h3>Hey there, {auth.user.displayName}! We've been expecting you.</h3>
+      <button onClick={auth.signOut}>Go Back</button>
+    </div>
+  );
+
+  console.log(auth.user);
 
   return (
-    <div>
-      <h1>you ARE authorized</h1>
-      <button onClick={auth.signOut}>sign out</button>
-    </div>
+    <PageContainer>
+      <CodeContainer>
+        {auth.authorized ? authorizedUI() : unauthorizedUI()}
+      </CodeContainer>
+    </PageContainer>
   );
 };
 
